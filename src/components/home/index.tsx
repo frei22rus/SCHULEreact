a015f77c38073615a10bd0086c6a7760f33426bc
frei22rus <frei22rus@gmail.com>
2024-02-27 22:30:55 +0700

    27/02

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,10 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-        <h1>Домашняя страница</h1>
+      <h1>Домашняя страница</h1>
     </div>
   );
-}
+};
 
 export default Home;
-  
